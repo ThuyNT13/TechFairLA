2ddfc2776e6f95c1613a28e0f9c4fee9602bf0b4
kyle kenney <kylekenney@kyles-MacBook-Pro.local>
2018-03-07 10:53:15 -0800

    simple nav component created

---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Navbar from './Navbar';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar title="Personality" />
+            </div>
+        );
+    }
+}
