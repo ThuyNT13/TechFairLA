b9e13603f28d7426109d69e482fb56daa60e93aa
kyle kenney <kylekenney@kyles-MacBook-Pro.local>
2018-03-07 22:38:15 -0800

    form page layout cleanup and brand title links to home page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class NavBar extends Component {
     render() {
         return (
                 <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
-                    <a className="navbar-brand" href="#">{this.props.title}</a>
+                    <Link to="/" className="navbar-brand" href="#">{this.props.title}</Link>
                     <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                        <input className="form-control mr-sm-2" type="search" placeholder="I don't work right now" aria-label="Search" />
                             <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                     </form>
                 </nav>
